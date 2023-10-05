8a3a14ed7bec8a6be58018176a8827ca740de8b9
Ethan Vo <ethan.vo@hotmail.com>
2023-10-05 09:15:03 -0400

    disk based storage fix

--- 01_peommp2_cbe/support.py
+++ 01_peommp2_cbe/support.py
@@ -9,6 +9,13 @@ class _ERIS:
     def __init__(self, cc):
         pass
 
+def load_h5(inputfile, key):
+    f = h5py.File(inputfile, "r")
+    data = {}
+    for k, v in f[key].items():
+        data[k] = v[:]
+    return data
+
 def load_mf(inputfile):
     material = load("data/{}".format(inputfile))
     cell, scfdata = chkfile.load_scf("data/{}".format(material["chk"]))  # input
@@ -25,7 +32,7 @@ def load_mp(inputfile):
     mymf = load_mf(inputfile)
     mycc = cc.KRCCSD(mymf, frozen=material["frozen"])
     mycc.keep_exxdiv = True
-    t_amps_dict = chkfile.load("data/{}".format(material["imds"]), "t_amps")
+    t_amps_dict = load_h5("data/{}".format(material["imds"]), "t_amps")
     mycc.__dict__.update(t_amps_dict)
     mycc.converged = True
     return mycc
@@ -34,7 +41,7 @@ def load_eris(inputfile):
     material = load("data/{}".format(inputfile))
     mycc = load_mp(inputfile)
     eris = _ERIS(mycc)
-    eris_dict = chkfile.load("data/{}".format(material["imds"]), "eris")
+    eris_dict = load_h5("data/{}".format(material["imds"]), "eris")
     eris.__dict__.update(eris_dict)
     return eris
 
@@ -42,10 +49,10 @@ def load_imds(inputfile):
     material = load("data/{}".format(inputfile))
     mycc = load_mp(inputfile)
     eris = _ERIS(mycc)
-    eris_dict = chkfile.load("data/{}".format(material["imds"]), "eris")
+    eris_dict = load_h5("data/{}".format(material["imds"]), "eris")
     eris.__dict__.update(eris_dict)
     imds = _IMDS(mycc, eris)
-    imds_dict = chkfile.load("data/{}".format(material["imds"]), "imds")
+    imds_dict = load_h5("data/{}".format(material["imds"]), "imds")
     imds.__dict__.update(imds_dict)
     imds.Wooov = imds.eris.ooov
     return imds
