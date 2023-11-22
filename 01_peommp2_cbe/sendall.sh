ad516cb902150660b0e627910e9ec08a205e6893
Ethan Vo <ethan.vo@hotmail.com>
2023-11-22 10:02:37 -0500

    silicon

--- 01_peommp2_cbe/sendall.sh
+++ 01_peommp2_cbe/sendall.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-./submit.sh c_ccpcvtz_k3_C_1s_nvir_act25 716800 5-00:00:00
-./submit.sh c_ccpcvtz_k2_C_1s_nvir_act25 716800 5-00:00:00
+./submit.sh si_ccpcvtz_k4_Si_2p_nvir_act11 716800 5-00:00:00
+./submit.sh si_ccpcvtz_k3_Si_2p_nvir_act11 716800 5-00:00:00
+./submit.sh si_ccpcvtz_k2_Si_2p_nvir_act11 716800 5-00:00:00
+./submit.sh si_ccpcvtz_k3_Si_2p 716800 5-00:00:00
+./submit.sh si_ccpcvtz_k2_Si_2p 716800 5-00:00:00
