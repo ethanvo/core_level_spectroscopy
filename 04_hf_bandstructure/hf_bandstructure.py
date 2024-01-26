4814f0a9d860203116eeb0430478ddc2d45bbc78
Ethan Vo <ethan.vo@hotmail.com>
2024-01-26 15:51:06 -0500

    pseudopotential and bandstructures

--- 04_hf_bandstructure/hf_bandstructure.py
+++ 04_hf_bandstructure/hf_bandstructure.py
@@ -5,7 +5,7 @@ from fileutils import load
 
 input_file = sys.argv[1]
 data = load("data/{}".format(input_file))
-cell = make_cell(data["formula"], data["basis"])
+cell = make_cell(data["formula"], data["basis"], pseudo=data["pseudo"])
 vbmax_kpt, cbmin_kpt, g_vbmax, g_cbmin = get_all_electron_bandstructure(data["formula"], cell, data["path"], data["npoints"], data["kmesh"], e_kn_file=data["e_kn_file"], output_file=data["output_file"])
 print("vbmax_kpt: {}".format(vbmax_kpt))
 print("cbmin_kpt: {}".format(cbmin_kpt))
--- 04_hf_bandstructure/sendall.sh
+++ 04_hf_bandstructure/sendall.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sbatch -J zno_ccpcvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py zno_ccpcvtz_222_wide.json
-sbatch -J gan_ccpcvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gan_ccpcvtz_222_wide.json
-sbatch -J gaas_ccpcvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gaas_ccpcvtz_222_wide.json
-sbatch -J mno_ccpcvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py mno_ccpcvtz_222_wide.json
+sbatch -J zno_ccecpccpvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py zno_ccecpccpvtz_222_wide.json
+sbatch -J gan_ccecpccpvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gan_ccecpccpvtz_222_wide.json
+sbatch -J gaas_ccecpccpvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gaas_ccecpccpvtz_222_wide.json
+sbatch -J mno_ccecpccpvtz_222_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py mno_ccecpccpvtz_222_wide.json
+sbatch -J zno_ccecpccpvtz_333_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py zno_ccecpccpvtz_333_wide.json
+sbatch -J gan_ccecpccpvtz_333_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gan_ccecpccpvtz_333_wide.json
+sbatch -J gaas_ccecpccpvtz_333_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gaas_ccecpccpvtz_333_wide.json
+sbatch -J mno_ccecpccpvtz_333_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py mno_ccecpccpvtz_333_wide.json
+sbatch -J zno_ccecpccpvtz_444_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py zno_ccecpccpvtz_444_wide.json
+sbatch -J gan_ccecpccpvtz_444_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gan_ccecpccpvtz_444_wide.json
+sbatch -J gaas_ccecpccpvtz_444_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py gaas_ccecpccpvtz_444_wide.json
+sbatch -J mno_ccecpccpvtz_444_wide /burg/berkelbach/users/eav2136/builds/work_tools/slurm/pyscf/pyscf-job.sh hf_bandstructure.py mno_ccecpccpvtz_444_wide.json
